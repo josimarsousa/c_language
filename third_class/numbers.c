5af61dca924ce29276b79242cd2e1b29403513c9
josimarsousa <josimarsousa@gmx.com>
2025-10-22 10:36:00 -0300

    learning about numbers

---
+++ third_class/numbers.c
@@ -0,0 +1,17 @@
+/* numbers.c */
+#include <stdio.h>
+
+int main() {
+
+    int x, y;
+
+    printf("Please input a number: \n");
+    scanf("%d", x);
+
+    printf("Please input another number: \n");
+    scanf("%d", y);
+
+
+    return 0;
+
+}

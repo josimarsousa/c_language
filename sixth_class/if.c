984b2f44e4b1c1ff1a29df687a36978ef6825bce
josimarsousa <josimarsousa@gmx.com>
2025-10-23 04:20:20 -0300

    aggregating else to the statement if

--- sixth_class/if.c
+++ sixth_class/if.c
@@ -5,9 +5,25 @@
 int main (){
 
     int x = 5;
+    int velocity;
 
     if(x > 4){
         printf("the number %d is greather than 4\n", x);
+    }
+
+    printf("How fast are you driving in km/h? \n");
+    scanf("%d", &velocity);
+
+    if(velocity > 100){
+        printf("You are driving too fast, carefull!\n");
+    }
+
+    else if(velocity <= 80){
+        printf("You are driving safely!\n");
+    }
+
+    else {
+        printf("You are driving close to the safety limit!\n");
     }
 
     return 0;

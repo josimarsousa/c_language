42b629adbb0896ce9e4aee6591e933bd4e5bcbc3
josimarsousa <josimarsousa@gmx.com>
2025-10-30 08:33:37 -0300

    added variables and procedures

--- game/dice.c
+++ game/dice.c
@@ -5,35 +5,9 @@
 #include <stdlib.h>
 
 
-int ramdom(int max){
-    int x = rand() % max +1;
-
-    return x;
-}
-
 
 int main() {
 
-    int balance, bet, guess, dice;
-
-    srand(getpid());
-
-    balance = 1000;
-    printf("Make your bet: ");
-    fflush(stdout);
-    scanf("%d", &bet);
-
-    printf("Make your guess (1-6, and 0 means quit): \n");
-    scanf("%d", &guess);
-
-    if(guess == 0)
-    return 0;
-
-    sleep(2);
-    dice = myRandom(6);
-    printf("%d\n", dice);
-
-    return 0;
 
 
 }

65ea62e0cc25383a8f6e391cb43f1a4b46a3cb74
josimarsousa <josimarsousa@gmx.com>
2025-11-01 05:25:54 -0300

    simple struct complete

--- structures/struct.c
+++ structures/struct.c
@@ -1,17 +1,29 @@
 /* struct.c */
 
 #include <stdio.h>
+#include <string.h>
 
 struct person
 {
     char name[14];
     char lastname[14];
     int age;
-    
+
 };
-
-
 
 int main() {
 
+    struct person birch;
+    
+    strncpy(birch.name, "doctor", 13);
+    strncpy(birch.lastname, "birch", 13);
+    birch.age = 45;
+
+    printf("%s %s of the age %d\n",
+            birch.name,
+            birch.lastname,
+            birch.age);
+
+    return 0;
+    
 }
